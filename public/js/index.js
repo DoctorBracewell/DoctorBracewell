44037a69f395d5213a80a840426165b5177004fe
DrBracewell <dragonclawslice1@gmail.com>
2020-12-07 18:45:05 +0000

    basic setup

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,66 @@
-for (let element of document.querySelectorAll(".navbar-link")) {
-    element.addEventListener("click", () => {
-        document.querySelector(`#${element.innerHTML}`).scrollIntoView({block: "start"});
-    });
+/* Particles Loading */
+let backgroundParticles, downParticles;
+
+function generateParticles() {
+  const SCREEN_AVERAGE = (window.innerHeight + window.innerWidth) / 2;
+  
+  const backgroundParticleOptions = {
+    "fpsLimit": 60,
+    "particles": {
+        "color": {
+        "value": "#141617"
+        },
+        "links": {
+            "color": {
+                "value": "#FAF0F0"
+            },
+            "distance": SCREEN_AVERAGE / 10,
+            "enable": true,
+            "opacity": 0.4
+        },
+        "move": {
+            "attract": {
+                "rotate": {
+                "x": 600,
+                "y": 1200
+                }
+            },
+            "enable": true,
+            "outModes": {
+                "bottom": "out",
+                "left": "out",
+                "right": "out",
+                "top": "out"
+            }
+        },
+        "opacity": {
+            "value": 0.9
+        },
+        "shape": {
+            "options": {
+                "polygon": {
+                    "nb_sides": 3
+                }
+            }
+        },
+        "number": {
+            "value": SCREEN_AVERAGE / 15
+        },
+        "size": {
+            "value": 0,
+        }
+    }
+  }
+
+  backgroundParticles = tsParticles.load("backgroundParticles", backgroundParticleOptions);
 }
 
-if (localStorage.getItem('cookies') === null) {
-    document.querySelector("#cookies-popup").style.display = "flex";
-}
+window.addEventListener("load", generateParticles);
+window.addEventListener("resize", generateParticles);
 
-document.querySelector(".cookies-accept").addEventListener("click", () => {
-    localStorage.setItem("cookies", true);
-    document.querySelector("#cookies-popup").style.display = "none";
+/* Link Scrolloing */
+
+document.querySelector("#aboutLink").addEventListener("click", () => {
+  document.querySelector("#about").scrollIntoView({behavior: "smooth"});
 })
+  
